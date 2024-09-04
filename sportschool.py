d47a0967dbd0d3bd7eb304f1652eb9548e4269ad
bas8334 <50291535+bas8334@users.noreply.github.com>
2024-09-05 01:00:55 +0200

    Update sportschool.py

--- sportschool.py
+++ sportschool.py
@@ -8,10 +8,13 @@ st.title('Data verzenden naar een Webhook')
 
 # Aantal rijen en kolommen
 num_rows = 15
-num_cols = 4
+num_cols = 6
 
-# Creëer een lege DataFrame met de juiste afmetingen
-data = pd.DataFrame('', index=range(num_rows), columns=[f'Kolom {i+1}' for i in range(num_cols)])
+# Definieer de kolomnamen
+kolom_namen = ['Oefening', 'Set 1 (#xKG)', 'Set 2 (#xKG)', 'Set 3 (#xKG)', 'Set 4 (#xKG)', 'Set 5 (#xKG)']
+
+# Creëer een lege DataFrame met de juiste kolomnamen
+data = pd.DataFrame('', index=range(num_rows), columns=kolom_namen)
 
 # Naam_sporter invoerveld
 naam_sporter = st.text_input('Naam van de sporter', value='')
@@ -23,7 +26,7 @@ datum_vandaag = datetime.now().strftime('%d-%m-%Y')
 for i in range(num_rows):
     cols = st.columns(num_cols)
     for j in range(num_cols):
-        data.iloc[i, j] = cols[j].text_input(f'Rij {i+1} - Kolom {j+1}', value='', key=f'input_{i}_{j}')
+        data.iloc[i, j] = cols[j].text_input(f'Rij {i+1} - {kolom_namen[j]}', value='', key=f'input_{i}_{j}')
 
 # URL van de webhook
 webhook_url = st.text_input('Voer de Webhook URL in', 'https://example.com/webhook')
